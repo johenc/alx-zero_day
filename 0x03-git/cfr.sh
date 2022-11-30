3ebbb4a5efa3efca4bddb3b3115def81e86b970c
johenc <johenclark@gmail.com>
2022-11-30 14:24:54 -0800

    Starting to code today, so cool

---
+++ 0x03-git/cfr.sh
@@ -0,0 +1,38 @@
+#! /bin/sh
+mkdir bash
+
+mkdir c
+
+mkdir js
+
+ls
+
+touch c/c_is_fun.c 
+
+touch js/main.js 
+
+touch js/index.js
+
+cd c 
+
+ls
+
+cd ..
+        
+cd bash 
+
+echo ‘#!/bin/bash’ >alx 
+
+echo “ALX” >>alx 
+    
+echo ‘#!/bin/bash’>school
+
+echo “School”>>school
+
+cd .. 
+
+git add .
+	     
+git commit -m 'Starting to code today, so cool'
+
+git push
